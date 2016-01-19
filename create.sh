bb5f42400a28023fd5ea4d50a50b6de99b13adaf
roman <rsng@mail.ru>
2016-01-19 16:20:34 +0300

    Modify schema. WB-31.

--- create.sh
+++ create.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 # curl -XDELETE 'http://localhost:9200/wobot'
 curl -XPOST 'http://localhost:9200/wobot' -d @mapping.json
 # curl -XGET 'http://localhost:9200/wobot/_mapping?pretty'
 
-curl -XPOST 'http://localhost:9200/wobot/profile/kviz777' -d @profile-sample.json
-curl -XPOST 'http://localhost:9200/wobot/post/post457?parent=kviz777' -d @post-sample.json
+curl -XPOST 'http://localhost:9200/wobot/profile/vk3000' -d @profile-sample.json
+curl -XPOST 'http://localhost:9200/wobot/post/vk897?parent=vk3000' -d @post-sample.json
